59e0c0d667bd2d35921e549385d8d56d0059a830
Özgün Doğan <ozgundgn0@gmail.com>
2023-05-03 16:46:09 +0300

    basic finding songs feature added

--- lib/services/api/genius/genius_service.dart
+++ lib/services/api/genius/genius_service.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:songlyrics/models/song.dart';
 import 'package:songlyrics/services/api/abstract/api_provider.dart';
 import 'package:http/http.dart' as http;
@@ -5,14 +6,17 @@ import '../../../helpers/api_urls.dart';
 
 class GeniusService implements ApiProvider {
   @override
-  /* Future<Iterable<Song>> */ void getSongsByLyrics(
-      {required String text}) async {
-    final response = await http
-        .get(Uri.parse(GeniusApi.http + '/search?q=' + text), headers: {
+  Future<Iterable<Song>> getSongsByLyrics({required String text}) async {
+    final response =
+        await http.get(Uri.parse('${GeniusApi.http}search?q=$text'), headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'Authorization': 'Bearer ${GeniusApi.token}',
     });
-    if (response.statusCode == 200) {}
+
+    var body = jsonDecode(response.body);
+    var hits = body["response"]["hits"];
+    var songList = hits.map<Song>((json) => Song.fromJson(json)).toList();
+    return songList;
   }
 }
