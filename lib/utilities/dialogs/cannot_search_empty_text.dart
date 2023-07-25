37296f4271433e91a5a9e964402bee8c379311f5
Özgün Doğan <ozgundgn0@gmail.com>
2023-07-25 23:48:38 +0300

    give some words context added

--- lib/utilities/dialogs/cannot_search_empty_text.dart
+++ lib/utilities/dialogs/cannot_search_empty_text.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:songlyrics/extensions/buildcontext/loc.dart';
 
 import 'generic_dialog.dart';
 
 Future<void> showCannotSearchTextDialog(BuildContext context) {
   return showGenericDialog<void>(
       context: context,
-      content: "Lütfen boş yapmayın",
-      title: "Uyarı",
+      content: context.loc.please_give_some_words,
+      title: context.loc.warning,
       optionsBuilder: () => {
             'OK': null,
           });
